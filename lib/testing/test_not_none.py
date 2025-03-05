1bd3c08e4c7933b7fa7d6c1aa536df05497b7e6d
Nzile22 <diananduku00@gmail.com>
2025-03-05 20:41:47 +0300

    initial commit

--- lib/testing/test_not_none.py
+++ lib/testing/test_not_none.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
-
+import pytest
 from not_none_functions import return_not_none
 
-# def test_return_not_none():
-#     '''in not_none_functions, function "return_not_none" returns a value that is not None.'''
-#     assert False
+def test_return_not_none():
+    '''In not_none_functions, function "return_not_none" returns a value that is not None.'''
+    result = return_not_none()
+    assert result is not None  # Check that the result is not None
